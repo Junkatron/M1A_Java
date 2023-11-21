f0874de46a128ecef657a10ac9b20575be24c8a3
Jas <junktrunk890@hotmail.com>
2023-11-21 10:01:36 +0000

    lab 3

--- labs/src/lab03/Program.java
+++ labs/src/lab03/Program.java
@@ -1,10 +1,26 @@
 package lab03;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int aNumber = getInt("Enter an Integer: ");
+		String text = getString("Enter a String: ");
+		
+		System.out.println("Your entered: " + aNumber + " AND " + text);
+	}
+	
+	public static int getInt(String prompt) {
+		System.out.println(prompt);
+		Scanner s = new Scanner(System.in);
+			return s.nextInt();
 	}
 
+	public static String getString(String prompt ) {
+		System.out.println(prompt);
+		Scanner s = new Scanner(System.in);
+			return s.nextLine();
+	}
 }
