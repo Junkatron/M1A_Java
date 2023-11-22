3231b2a9d5405ca10a8b78fd14d9231a13b406cc
Jas <junktrunk890@hotmail.com>
2023-11-22 15:53:17 +0000

    Lab 9 part 1

---
+++ labs/src/lab09/Program.java
@@ -0,0 +1,44 @@
+package lab09;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// Lab 9 part 1
+		Map map = new Map(500, 400);
+		Token t1 = new Token(70, 30, map);
+		Token t2 = new Token(10, 20, map);
+		Token t3 = new Token(100, 200, map);
+		
+		displayCoord(t1, t2, t3);
+		
+		System.out.println("Move t1 east by 100");
+		t1.move("east", 100);
+		displayCoord(t1, t2, t3);
+
+		System.out.println("Move t1 west by 50");
+		t1.move("WEST", 50);
+		displayCoord(t1, t2, t3);
+		
+		System.out.println("Move t1 west by 500");
+		if (!t1.move("W", 500))
+			t1 = null;
+		displayCoord(t1, t2, t3);
+		
+		System.out.println("Move t2 east(90) by 100");
+		if (!t2.move(90, 100))
+			t2 = null;
+		displayCoord(t1, t2, t3);
+	}
+
+	private static void displayCoord(Token t1, Token t2, Token t3) {
+		String tokenCoordText = "Token %s is at: x=%d, y=%d\n";
+		if (t1 != null)
+			System.out.printf(tokenCoordText, "t1", t1.getX(), t1.getY());
+		if (t2 != null)
+			System.out.printf(tokenCoordText, "t2", t2.getX(), t2.getY());
+		if (t3 != null)
+			System.out.printf(tokenCoordText, "t3", t3.getX(), t3.getY());
+		System.out.println();
+	}
+
+}
---
+++ labs/src/lab09/Token.java
@@ -0,0 +1,86 @@
+package lab09;
+
+public class Token {
+	private int x;
+	private int y;
+	private Map map;
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public Map getMap() {
+		return map;
+	}
+	public void setMap(Map map) {
+		this.map = map;
+	}
+	
+	public Token(int x, int y, Map map) {
+		this.x = x;
+		this.y = y;
+		this.setMap(map);
+	}
+	
+	boolean move(int directionInt, int distance) {
+		String direction = null;
+		switch(directionInt) {
+			case 0:
+				direction = "north";
+				break;
+			case 90:
+				direction = "east";
+				break;
+			case 180:
+				direction = "south";
+				break;
+			case 270:
+				direction = "west";
+				break;
+		}
+		return move(direction, distance);
+	}
+	
+	boolean move(String direction, int distance) {
+		boolean isValidMove = false;
+		String invalidMoveText = "The token has fallen off the map";
+
+		switch(direction.toLowerCase()) {
+			case "north": case "n":
+				if (getY() + distance < map.getySize()) {
+					setY(getY() + distance);
+					isValidMove = true;
+				} 
+				break;
+			case "south": case "s":
+				if (getY() - distance >= 0) {
+					setY(getY() - distance);
+					isValidMove = true;
+				}
+				break;
+			case "east": case "e":
+				if (getX() + distance < map.getxSize()) {
+					setX(getX() + distance);
+					isValidMove = true;
+				}
+				break;
+			case "west": case "w":
+				if (getX() - distance >= 0) {
+					setX(getX() - distance);
+					isValidMove = true;
+				}
+				break;
+		}
+		if (!isValidMove) {
+			System.out.println(invalidMoveText);
+		}
+		return isValidMove;
+	}
+}
